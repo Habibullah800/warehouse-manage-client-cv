b2d117f883d264910c5bdee9e5e2eb9d4227b90b
Habibullah800 <mdhabibullah800@gmail.com>
2022-05-08 13:07:01 +0600

    add item form added

--- src/Component/Home/AddItem/AddItem.js
+++ src/Component/Home/AddItem/AddItem.js
@@ -6,10 +6,12 @@ import useFirebase from '../../Hooks/UseFirebase';
 import "./AddItem.css"
 
 
+
 const AddItem = () => {
     const { register, handleSubmit } = useForm();
     const onSubmit = data => console.log(data);
     const { user } = useFirebase()
+
     return (
         <div className='addItem'>
             <h2 className='mb-5 mt-4'>Here you can add any Item</h2>
@@ -17,12 +19,23 @@ const AddItem = () => {
             {
                 user?.uid
                     ?
-                    <div >
-                        <form onSubmit={handleSubmit(onSubmit)}>
-                            <input{...register("firstName", { require: true, maxLength: 20 })}></input>
-                            <input{...register("firstName", { require: true, maxLength: 20 })}></input>
-                            <input{...register("firstName", { require: true, maxLength: 20 })}></input>
-                            <input type="submit"></input>
+                    <div className=' mx-auto form' >
+                        <form className='d-flex flex-column' onSubmit={handleSubmit(onSubmit)}>
+                            <input className='mb-3 ps-3' type='text' placeholder='Product Name' {...register("name", { require: true, maxLength: 20 })}></input>
+
+                            <textarea className='mb-3 ps-3' type='text' placeholder='Short Description' {...register("description", { require: true, maxLength: 60 })}></textarea>
+
+                            <input className='mb-3 ps-3' type='text' placeholder='Supplier Name' {...register("supplier", { require: true, maxLength: 60 })}></input>
+
+                            <input className='mb-3 ps-3' type='number' placeholder='Price' {...register("price", { require: true, maxLength: 60 })}></input>
+
+                            <input className='mb-3 ps-3' type='number' placeholder='Quantity' {...register("quantity", { require: true, maxLength: 60 })}></input>
+
+
+                            <input className='mb-3 ps-3' type='text' placeholder='Image URL' {...register("img", { require: true, maxLength: 20 })}></input>
+
+
+                            <input className='w-50 mx-auto btn btn-warning' type="submit" ></input>
 
                         </form>
                     </div>
